522adda6d9b47f9b51c2f0e2875578f191331573
Saira Viqar <sairaviqar@gmail.com>
2018-01-02 13:24:13 +1000

    iterating over files

--- dev/script.py
+++ dev/script.py
@@ -4,20 +4,27 @@ import sys
 import csv
 import collections
 import pprint
+import os
 
-with open('../data/old/t1.csv') as csvfile:
-    readCSV = csv.reader(csvfile, delimiter=',')
-    result = collections.defaultdict(list)
-    for row in readCSV:
-        #print (', '.join(row))
-        year = row[0].split(" ")[0]
-        result[year].append(row)
 
-header=result.get('TIMESTAMP')        
-for i,j in result.items():
-    file_path = "%s.csv"%(i)
-    with open(file_path, 'w') as fp:
-        writer = csv.writer(fp, delimiter=',')
-        writer.writerows(header) 
-        writer.writerows(j)       
+directory = "../data/old/"
+
+for filename in os.listdir(directory):
+    print(filename)
+    with open(directory+filename) as csvfile:
+        if filename.endswith(".csv"):
+            readCSV = csv.reader(csvfile, delimiter=',')
+            result = collections.defaultdict(list)
+            for row in readCSV:
+                #print (', '.join(row))
+                year = row[0].split(" ")[0]
+                result[year].append(row)
+       
+            header=result.get('TIMESTAMP')        
+            for i,j in result.items():
+                file_path = "%s%s.csv"%(filename, i)
+                with open(file_path, 'w') as fp:
+                    writer = csv.writer(fp, delimiter=',')
+                    writer.writerows(header) 
+                    writer.writerows(j)       
         
