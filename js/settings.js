50ca86665f0d7da6cd51cfb00e8846136b85ea17
Saira Viqar <sairaviqar@gmail.com>
2018-01-10 17:04:13 +1000

    settings changed

---
+++ js/settings.js
@@ -0,0 +1,34 @@
+
+
+
+/*
+
+var configSettings= {
+
+
+
+}
+
+
+
+
+$( "#old" ).click(function() {
+    configSettings.defaultCharts="old";
+});
+
+$( "#current" ).click(function() {
+    configSettings.defaultCharts="current";
+});
+
+
+
+
+
+$( "#save" ).click(function() {
+    alert( "Handler for .click() called." );
+});
+
+
+$( "#apply" ).click(function() {
+    alert( "Handler for .click() called." );
+});*/
