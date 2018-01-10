b6c8866aa62ed3de3fb4c81e13f5004256733c19
Saira Viqar <sairaviqar@gmail.com>
2018-01-10 21:34:13 +1000

    settings changed

--- js/settings.js
+++ js/settings.js
@@ -50,25 +50,6 @@ $( "#current" ).click(function() {
 
 $( "#save" ).click(function() {
 
-    for(var i=1; i<7; i++) {
-        configSettings.locations[i-1].name=($("input[type=text][id=name-"+i+"]").val());
-
-        configSettings.locations[i-1].lat=$("input[type=text][id=lat-"+i+"]")
-            .val( );
-
-        configSettings.locations[i-1].lon=$("input[type=text][id=lon-"+i+"]")
-            .val( );
-
-        configSettings.locations[i-1].label=$("input[type=text][id=label-"+i+"]")
-            .val( );
-
-        configSettings.locations[i-1].chartHeading=$("input[type=text][id=heading-"+i+"]")
-            .val();
-
-        configSettings.locations[i-1].chartSubheading=$("input[type=text][id=subheading-"+i+"]")
-            .val( );
-    }
-
     //create a temp file for these values
 
 });
