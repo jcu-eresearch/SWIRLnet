9d25f9ce9bfceae438765706cabf16fb7bc5d20d
Saira Viqar <sairaviqar@gmail.com>
2018-04-10 19:13:38 +1000

    Added preview scripts

---
+++ dev/dev-preview/images-preview.py
@@ -0,0 +1,102 @@
+#!/usr/bin/python3
+
+import sys
+import csv
+import datetime
+import collections
+import pprint
+import os
+import json
+
+
+unprocessedDir= "/srv/SWIRLnet/data-preview/"
+dataDir ="/srv/SWIRLnet/data-preview/timelapse"
+configDir="/srv/SWIRLnet/config/"
+
+
+def checkDate(inputDate):
+    if inputDate == "":
+        return False
+    isValid= True;
+    try:
+        datetime.datetime.strptime( inputDate, "%Y-%m-%d %H:%M:%S" )
+    except ValueError :
+        isValid = False
+    return isValid
+
+with open(configDir+"config-preview.json", encoding='UTF-8') as configFile:
+    text = configFile.read()
+    config = json.loads(text)
+    jsonDates = config["cameraDateRanges"]
+
+    for item in jsonDates:
+
+        oldStart = item.get("oldStart")
+        oldEnd= item.get("oldEnd")
+        newStart = item.get("newStart")
+        newEnd= item.get("newEnd")
+
+        oldDir = item.get("oldDir")
+        newDir= item.get("newDir")
+
+
+
+
+        if not checkDate(oldStart) :
+            oldEnd = datetime.datetime.now()
+            oldStart = oldEnd - datetime.timedelta(days=3)
+        elif not checkDate(oldEnd) :
+            oldStart=datetime.datetime.strptime(oldStart, "%Y-%m-%d %H:%M:%S")
+            oldEnd= oldStart + datetime.timedelta(days=3)
+        else :
+            oldStart=datetime.datetime.strptime(oldStart, "%Y-%m-%d %H:%M:%S")
+            oldEnd=datetime.datetime.strptime(oldEnd, "%Y-%m-%d %H:%M:%S")
+
+        if not checkDate(newStart) :
+            newEnd = datetime.datetime.now()
+            newStart = newEnd - datetime.timedelta(days=3)
+        elif not checkDate(newEnd) :
+            newStart=datetime.datetime.strptime(newStart, "%Y-%m-%d %H:%M:%S")
+            newEnd= newStart + datetime.timedelta(days=3)
+        else :
+            newStart=datetime.datetime.strptime(newStart, "%Y-%m-%d %H:%M:%S")
+            newEnd=datetime.datetime.strptime(newEnd, "%Y-%m-%d %H:%M:%S")
+
+
+
+        for filename in os.listdir(unprocessedDir+oldDir):
+            try:
+                fileDir= dataDir+oldDir
+                if filename.endswith(".jpg") and len(filename)>22:
+                    l= len(filename)
+                    date=datetime.datetime.strptime(filename[l-23: l-4], "%Y_%m_%d_%H_%M_%S")
+                    if date>=oldStart and date<=oldEnd :
+                        if not os.path.exists(os.path.dirname(fileDir)):
+                            try:
+                                os.makedirs(os.path.dirname(fileDir))
+                            except OSError as exc: # Guard against race condition
+                                if exc.errno != errno.EEXIST:
+                                    raise
+                        os.rename(unprocessedDir+oldDir+filename, fileDir+filename)
+
+            except Exception as inst:
+                pass
+
+
+        for filename in os.listdir(unprocessedDir+newDir):
+            try:
+                fileDir= dataDir+newDir
+                if filename.endswith(".jpg") and len(filename)>22:
+                    l= len(filename)
+                    date=datetime.datetime.strptime(filename[l-23: l-4], "%Y_%m_%d_%H_%M_%S")
+                    if date>=newStart and date<=newEnd :
+                        if not os.path.exists(os.path.dirname(fileDir)):
+                            try:
+                                os.makedirs(os.path.dirname(fileDir))
+                            except OSError as exc: # Guard against race condition
+                                if exc.errno != errno.EEXIST:
+                                    raise
+                        os.rename(unprocessedDir+newDir+filename, fileDir+filename)
+                        
+            except Exception as inst:
+                pass
